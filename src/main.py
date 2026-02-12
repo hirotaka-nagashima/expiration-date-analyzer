87000445893fe45d7b8b4dff0a3b47e07155eb5f
Hirotaka Nagashima <62758713+hirotaka-nagashima@users.noreply.github.com>
2026-02-12 23:52:39 +0900

    docs: Describe usage in main.py

--- src/main.py
+++ src/main.py
@@ -1,18 +1,37 @@
+from analyzer import timeextractor
+from analyzer.labeler import labeler
+from estimator import commander
 from logger import fileio, logger
 from twitter import authenticator
 
 
 def main():
-    def generate_logger(data_dir):
-        auth = authenticator.Authenticator("./twitter_credentials.json")
-        api = auth.authenticate()
-        io = fileio.CSVHandler(data_dir)
-        return logger.Logger(api, io)
+    credentials_path = "../credentials/twitter_credentials.json"
+    data_dir = "../data/ja"
+    tweets_lang = "ja"
 
-    data_dir = "../data/production/ja"
-    from estimator import commander
+    # 1. Logging of tweets
+    auth = authenticator.Authenticator(credentials_path)
+    api = auth.authenticate()
+    io = fileio.CSVHandler(data_dir)
+    log = logger.Logger(api, io)
+    log.track_dynamics(
+        loop_cycle=60,
+        max_num_ids_to_track=100,
+        logs_values=True,
+        logs_retweets=False,
+        lang=tweets_lang,
+        min_retweets=50,
+        additional_q="",
+    )
 
-    commander.do(data_dir)
+    # 2. Estimation of expiration date using dynamics
+    # 3. Estimation of expiration date using word co-occurrence
+    timeextractor.extract_time_expressions(data_dir, lang=tweets_lang)  # preprocessing
+    commander.do(data_dir)  # dataset construction (2), training & evaluation (3)
+
+    # 4. To assist users to label tweets expiration date
+    labeler.HandLabeler.run(data_dir)
 
 
 if __name__ == "__main__":
